e76ee7cb7fa9eb0a63c2cdedcbca68fb152325b0
RothaSAMON <samonrotha@gmail.com>
2025-03-27 09:33:23 +0700

    Finished auth with sanctum

--- routes/api.php
+++ routes/api.php
@@ -1,13 +1,20 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\CustomerController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
 
+// Auth routes
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/logout', [AuthController::class,'logout'])->middleware('auth:sanctum');
+
+// CRUD routes
 Route::get('/customers', [CustomerController::class, 'index']);
 Route::get('customers/{id}', [CustomerController::class, 'show']);
 Route::post('/customers', [CustomerController::class, 'store']);
